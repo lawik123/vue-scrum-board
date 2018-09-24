2f3fdf148d4e9fcba36c6a41a6836ec7b94f0da9
Lawik <428302@student.saxion.nl>
2018-09-24 14:24:14 +0200

    lint fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import BoardsOverview from '../components/BoardsOverview'
-import Board from '../components/scrumboard/Board'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import BoardsOverview from '../components/BoardsOverview.vue';
+import Board from '../components/scrumboard/Board.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    { 
-       path: '/',
-       component: BoardsOverview,
-    },
-    { 
-        path: '/board/:board',
-        name: 'board',
-        component: Board,
-        props: true,
-     }
-]
+  {
+    path: '/',
+    component: BoardsOverview,
+  },
+  {
+    path: '/board/:board',
+    name: 'board',
+    component: Board,
+    props: true,
+  },
+];
 
 // eslint-disable-next-line no-new
 const router = new VueRouter({
-   routes
-})
+  routes,
+});
 
-export default router
+export default router;
